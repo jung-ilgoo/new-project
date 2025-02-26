630200147e68f4620ab805f4f4e9d555b2aa6bd1
jung-ilgoo <lithography@kakao.com>
2025-02-26 11:44:37 +0900

    자동 배포 테스트

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Fly.io! This is a new project."
+    return "Hello, Fly.io! 🚀 자동 배포 테스트 완료!"
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
