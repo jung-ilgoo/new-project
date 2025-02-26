34ad324f6fde2d68653aa9b480357f1e640683a9
jung-ilgoo <lithography@kakao.com>
2025-02-26 14:41:58 +0900

    Add index.html as first page

--- app.py
+++ app.py
@@ -1,18 +1,16 @@
 import os
 import psycopg2
-from flask import Flask
+from flask import Flask, render_template, Blueprint
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    # Fly.io에서 자동 설정된 환경 변수 사용 (DATABASE_URL)
-    conn = psycopg2.connect(os.environ["DATABASE_URL"])
-    with conn.cursor() as cur:
-        cur.execute("SELECT 'Hello from PostgreSQL!'")
-        row = cur.fetchone()
-    conn.close()
-    return row[0]
+main_bp = Blueprint('main_bp', __name__)
+
+@main_bp.route("/")
+def home():
+    return render_template("index.html")
+
+app.register_blueprint(main_bp)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
